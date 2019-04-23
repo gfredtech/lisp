f89378813460718aadafa4b28130f97aaeec7842
gfredtech <gfredtech@gmail.com>
2019-04-23 17:22:44 +0000

    Initial commit

---
+++ src/prompt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+int main(int argc, char** argv) {
+  puts("Lispy Version 0.1");
+  puts("Press Ctrl+C to Exit\n");
+
+  while(1) {
+    char*input = readline("lispy >");
+
+    add_history(input);
+
+    printf("I got: %s\n", input);
+
+    free(input);
+  }
+
+  return 0;
+}
